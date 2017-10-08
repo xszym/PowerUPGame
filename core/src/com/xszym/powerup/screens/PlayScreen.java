ba42d2987f27f51836ae96ad07caba4a928a1bc5
xszym <xszym69@gmail.com>
2017-10-08 15:18:12 +0200

    improve appearance of the code

--- core/src/com/xszym/powerup/screens/PlayScreen.java
+++ core/src/com/xszym/powerup/screens/PlayScreen.java
@@ -1,14 +1,11 @@
 package com.xszym.powerup.screens;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.xszym.powerup.PowerUPMain;
 import com.xszym.powerup.entitlies.Robot;
-import com.xszym.powerup.ui.iClickCallback;
 
 import com.badlogic.gdx.utils.Array;
 
@@ -18,9 +15,7 @@ import java.util.Random;
 public class PlayScreen extends AbstractScreen {
 
     private Image bgGame;
-    private Robot robot;
     private Array<Robot> robots;
-    private Vector3 touchedCoordinate;
 
     public PlayScreen(PowerUPMain game) {
         super(game);
@@ -31,45 +26,8 @@ public class PlayScreen extends AbstractScreen {
         initBG();
         initRobots();
         createRobots();
-       // initRobotButton();
-
-        touchedCoordinate = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
-        camera.unproject(touchedCoordinate);
-
     }
 
-
-
-
-    private void initRobots() {
-
-        robots = new Array<Robot>();
-
-        for (int i = 1; i <= 5; i++) {
-            robots.add(new Robot(i * (Robot.HEIGHT)));
-            stage.addActor(robots.get(i - 1));
-        }
-
-    }
-
-    private void createRobots() {
-
-        for (final Robot robot : robots) {
-            Random random = new Random();
-
-                robot.fly(random.nextInt() * 10,100);
-
-//            if (robot.collides(platfom.getBoundsPlatform())) {
-//
-//            }
-        }
-
-
-
-
-
-
-    }
 
     @Override
     public void show() {
@@ -87,36 +45,14 @@ public class PlayScreen extends AbstractScreen {
 
     private void update(float delta) {
 
-
         bgGame.setPosition(0, camera.position.y - (camera.viewportHeight / 2));
 
         checkRobotTouched(delta);
 
-
-
-        touchedCoordinate.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-        camera.unproject(touchedCoordinate);
-        //System.out.println("x: " + touchedCoordinate.x + " y: " + touchedCoordinate.y);
-
-
         camera.update();
     }
 
-    private void checkRobotTouched(float delta) {
-        for (final Robot robot : robots) {
 
-            robot.addListener(new InputListener() {
-                public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
-                    //Gdx.app.log("Example", "touch started at (" + x + ", " + y + ")");
-                    robot.remove();
-                    return false;
-                }
-            });
-
-            robot.updateRobot(delta);
-
-        }
-    }
 
     @Override
     public void resize(int width, int height) {
@@ -159,5 +95,41 @@ public class PlayScreen extends AbstractScreen {
         stage.addActor(bgGame);
     }
 
+
+    private void initRobots() {
+
+        robots = new Array<Robot>();
+
+        for (int i = 1; i <= 5; i++) {
+            robots.add(new Robot(i * (Robot.HEIGHT)));
+            stage.addActor(robots.get(i - 1));
+        }
+    }
+
+    private void createRobots() {
+
+        for (final Robot robot : robots) {
+            Random random = new Random();
+
+            robot.fly(random.nextInt() * 10,100);
+
+        }
+    }
+
+    private void checkRobotTouched(float delta) {
+        for (final Robot robot : robots) {
+
+            robot.addListener(new InputListener() {
+                public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                    //Gdx.app.log("Example", "touch started at (" + x + ", " + y + ")");
+                    robot.remove();
+                    return false;
+                }
+            });
+
+            robot.updateRobot(delta);
+
+        }
+    }
 
 }
--- core/src/com/xszym/powerup/ui/iClickCallback.java
+++
@@ -1,6 +0,0 @@
-package com.xszym.powerup.ui;
-
-public interface iClickCallback
-{
-    void onClick();
-}
